082e5a19b7e241ebde2fa7a37855381c8cda78b6
SHOKEN EZAKI <shoken4797@gmail.com>
2023-08-03 05:34:08 +0000

    create articleList

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,42 @@
+<template>
+ <div class="article-list">
+     <Article v-for="(article, index) in articles" :key="index" :authorId="article.userId" :timestamp="article.timestamp" :content="article.text" :category="article.category" />
+ </div>
+</template>
+
+<script>
+import Article from '@/components/Article.vue';
+export default {
+  name: 'ArticleList',
+  components: {
+      Article
+  },
+  computed: {
+      
+  },
+  props: {
+    articles: {
+        type: Array,
+        required: true,
+        default: []
+    }  
+  },
+  data() {
+      return {
+      }
+  }
+};
+</script>
+
+<style scoped >
+   .article-list {
+       width: 85%;
+       max-width: 560px;
+       margin: 0 auto;
+       background-color: white;
+       padding: 1rem 3rem;
+       display: flex;
+       flex-direction: column;
+       column-gap: .8rem;
+   }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div>
-    <div class="ui main container">
+    <div v-if="isLoading" class="ui segment">
+      <div class="ui active dimmer">
+        <div class="ui text loader">Loading</div>
+      </div>
+    </div>
+    
+    <div v-if="!isLoading" class="ui main container">
       <!-- 基本的なコンテンツはここに記載する -->
       <div class="ui segment">
       </div>
+      <!--<Article />-->
+      <ArticleList :articles=articles />
     </div>
   </div>
 </template>
@@ -15,12 +23,17 @@
 // import { baseUrl } from '@/assets/config.js';
 
 // const headers = {'Authorization' : 'mtiToken'};
+import Article from '@/components/Article.vue';
+import ArticleList from '@/components/ArticleList.vue';
+import {baseUrl} from '@/assets/config.js';
 
 export default {
   name: 'Home',
 
   components: {
    // 読み込んだコンポーネント名をここに記述する
+   Article,
+   ArticleList
   },
 
   data() {
@@ -36,8 +49,22 @@ export default {
         start: null,
         end: null,
       },
-      articles: [],
+      articles: [
+        {userId: "jjsj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "ccc", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jsjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jsjj", text: "展覧会に行きたい", category: "", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+        {userId: "jsjj", text: "展覧会に行きたい", category: "f", timestamp: 1691037872561},
+        {userId: "jjj", text: "展覧会に行きたい", category: "aaa", timestamp: 1691037872561},
+      ],
       iam: null,
+      isLoading: false,
+      token: window.localStorage.getItem('token')
     };
   },
   created() {
@@ -45,6 +72,8 @@ export default {
     if(!token) {
       this.$router.push({name: 'Login'})
     }
+    
+  // this.getArticles();
   },
   computed: {
   // 計算した結果を変数として利用したいときはここに記述する
@@ -54,11 +83,67 @@ export default {
   methods: {
     // Vue.jsで使う関数はここで記述する
     // isMyArticle(id) {}, // 自分の記事かどうかを判定する
-    // async getArticles() {}, // 記事一覧を取得する
+    async getArticles() {
+      const headers = {'Authorization': this.token};
+    
+      try {
+            /* global fetch */
+        this.isLoading = true;
+        const res = await fetch(baseUrl + `/articles`,  {
+          method: 'GET',
+          headers
+        });
+  
+        const text = await res.text();
+        const jsonData = text ? JSON.parse(text) : {}
+        console.log("jsonData: ", jsonData);
+  
+        // fetchではネットワークエラー以外のエラーはthrowされないため、明示的にthrowする
+        if (!res.ok) {
+          const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
+          throw new Error(errorMessage);
+        }
+        this.isLoading = false;
+        console.log("jsonData: ", jsonData);
+        this.articles = jsonData;
+        
+      } catch (e) {
+        // エラー時の処理
+        this.isLoading = false;
+        console.log("e: ", e);
+      }
+    }, // 記事一覧を取得する
     // async postArticle() {}, // 記事を作成する
     // async getSearchedArticles() {}, // 記事を検索する
-    // async deleteArticle(article) {}, // 記事を削除する
-    // convertToLocaleString(timestamp) {} // timestampをLocaleDateStringに変換する
+    async deleteArticle({userId, timestamp}) {
+      const headers = {'Authorization': this.token};
+    
+      try {
+            /* global fetch */
+        this.isLoading = true;
+        const res = await fetch(baseUrl + `/article?userId=userId&timestamp=timestamp`,  {
+          method: 'DELETE',
+          headers
+        });
+  
+        const text = await res.text();
+        const jsonData = text ? JSON.parse(text) : {}
+  
+        // fetchではネットワークエラー以外のエラーはthrowされないため、明示的にthrowする
+        if (!res.ok) {
+          const errorMessage = jsonData.message ?? 'エラーメッセージがありません';
+          throw new Error(errorMessage);
+        }
+        this.isLoading = false;
+        console.log("jsonData: ", jsonData);
+        this.articles = jsonData;
+        
+      } catch (e) {
+        // エラー時の処理
+        this.isLoading = false;
+        console.log("e: ", e);
+      }
+    }, 
   }
 }
 </script>
